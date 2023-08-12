119df0749376f70e0bf7ac094f59b01ac258e1da
dbey21 <125637145+dbey21@users.noreply.github.com>
2023-08-12 03:14:01 -0700

    updated mt experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School"
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <
+      div className="experience">
+      
+      <VerticalTimeline lineColor="blue" >
+        
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="2023-"
+        iconStyle={{background:"blue", color: "white"}}
+        icon={<SchoolIcon />}>
+
+        <h3 className="vertical-timeline-element--title">UC Berkeley coding bootcamp, Berkeley,CA</h3>
+        
+        <p>Certificate</p>
+      
+      </VerticalTimelineElement>
+      
+      </VerticalTimeline>
+      
+      <VerticalTimelineElement className="vertical-timeline-element--education" date="April 2023-May 2023" iconStyle={{background:"blue", color: "white"}}
+        
+        icon={<SchoolIcon />}>
+
+         <h3 className="vertical-timeline-element--title">Created a application that allows a user to save events for each hour of the day, using  HTML and CSS powered by jQuery. </h3>
+        
+        <p></p>
+        
+        </VerticalTimelineElement>
+
+      
+    </div>
+
+    
+  );
 }
 
-export default Experience
+export default Experience;
